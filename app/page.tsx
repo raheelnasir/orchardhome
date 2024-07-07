f2353ffc94366f18c4e1d8940aa0c6ae644aa0dd
iraheeel <iraheeel@gmail.com>
2024-07-07 13:40:31 +0500

    feature(app): fix typos

--- app/page.tsx
+++ app/page.tsx
@@ -13,7 +13,7 @@ export default function Home() {
           <img src="/home.jpg" alt="Background Image" className="absolute inset-0 w-full h-full object-cover z-0" />
           <div className="absolute inset-0 bg-gradient-to-t from-black to-transparent z-10"></div>
           <div className="relative z-20 p-8 text-center">
-            <h1 className="text-4xl font-bold mb-4">Services ORCHARD HOMESTAY</h1>
+            <h1 className="text-4xl font-bold mb-4">Welcome to ORCHARD HOMESTAY</h1>
             <p className="text-lg">Stay in paradise of nature with beautiful view and nearby locations. Empower your travel experience with our 24/7 service.</p>
           </div>
         </header>
