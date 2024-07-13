51bec6b4fcc36e8d6513fc014caa639c489d87af
iraheeel <iraheeel@gmail.com>
2024-07-13 13:04:30 +0500

    feature(logo): add logo and images

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,3 @@
-
 import Feature from '@/components/Feature';
 import Guide from '@/components/Guide';
 import Camp from '@/components/Camp';
@@ -6,15 +5,17 @@ import Hero from '@/components/Hero';
 import Contact from '@/components/Contact';
 import CatalogueSection from '@/components/CatalogueSection';
 import CatalogSwiperSection from '@/components/CatalogSwiperCection';
+import { useEffect, useState } from 'react';
 
 export default function Home() {
+  
   return (
     <>
       <div className=''>
-        <header className="relative h-[80vh] !bg-top flex items-end justify-center text-white">
-          <img src="/home.jpg" alt="Background Image" className="absolute inset-0 w-full h-full object-cover z-0" />
-          <div className="absolute inset-0 bg-gradient-to-t from-black to-transparent z-10"></div>
-          <div className="relative z-20 p-8 text-center">
+        <header className="relative h-[80vh] mb-20 !bg-top flex items-end justify-center text-white">
+          <img src="/boat.png" alt="Background Image" className="absolute inset-0 w-full h-full object-cover z-0" />
+          <div className="absolute inset-0 bg-gradient-to-t from-black to z-10"></div>
+          <div className="relative z-20 inset-0 p-8 text-center">
             <h1 className="md:text-4xl  text-xl font-bold mb-4">Welcome to ORCHARD HOMESTAY</h1>
             <p className="md:text-lg text-base">Stay in paradise of nature with beautiful view and nearby locations. Empower your travel experience with our 24/7 service.</p>
           </div>
